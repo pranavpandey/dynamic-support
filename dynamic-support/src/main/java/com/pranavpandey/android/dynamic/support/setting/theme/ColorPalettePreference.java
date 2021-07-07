1314ac9d3683c3f2d63c94c6ec6a2464cdabbbf0
Pranav Pandey <pranavpande92@gmail.com>
2021-07-08 02:47:39 +0530

    Add dynamic color palette preference

---
+++ dynamic-support/src/main/java/com/pranavpandey/android/dynamic/support/setting/theme/ColorPalettePreference.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2018-2021 Pranav Pandey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pranavpandey.android.dynamic.support.setting.theme;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.AttrRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.pranavpandey.android.dynamic.support.setting.base.DynamicSpinnerPreference;
+import com.pranavpandey.android.dynamic.support.theme.DynamicTheme;
+
+/**
+ * A {@link DynamicSpinnerPreference} to display and edit the settings for the dynamic
+ * color palette.
+ */
+public class ColorPalettePreference extends DynamicSpinnerPreference {
+
+    public ColorPalettePreference(@NonNull Context context) {
+        super(context);
+    }
+
+    public ColorPalettePreference(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ColorPalettePreference(@NonNull Context context,
+            @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onUpdate() {
+        super.onUpdate();
+
+        setVisibility(DynamicTheme.getInstance().getListener().isDynamicColor() ? VISIBLE : GONE);
+    }
+}
