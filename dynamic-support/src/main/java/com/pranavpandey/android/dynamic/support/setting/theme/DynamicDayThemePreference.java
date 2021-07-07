d31c4bf981700a02ceb3008062bb3d42feabbdc6
Pranav Pandey <pranavpande92@gmail.com>
2021-07-08 02:46:10 +0530

    Add app and day theme preferences

--- dynamic-support/src/main/java/com/pranavpandey/android/dynamic/support/setting/theme/DynamicDayThemePreference.java
+++ dynamic-support/src/main/java/com/pranavpandey/android/dynamic/support/setting/theme/DynamicDayThemePreference.java
@@ -23,51 +23,28 @@ import androidx.annotation.AttrRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.pranavpandey.android.dynamic.support.R;
 import com.pranavpandey.android.dynamic.support.model.DynamicAppTheme;
 import com.pranavpandey.android.dynamic.support.theme.DynamicTheme;
 import com.pranavpandey.android.dynamic.theme.Theme;
-import com.pranavpandey.android.dynamic.utils.DynamicSdkUtils;
-
-import java.util.Arrays;
 
 /**
- * A {@link DynamicThemePreference} to display the night theme settings.
+ * A {@link DynamicThemePreference} to display the day theme setting.
  *
- * @see Theme#NIGHT
- *
- * <p>It will automatically adjust the values according to the available API.
- * <p>{@link Theme.Night#BATTERY} will be available for API 21 and above.
- * <p>{@link Theme.Night#SYSTEM} will be available for API 28 and above.
+ * @see Theme#DAY
  */
-public class DynamicNightThemePreference extends DynamicThemePreference {
+public class DynamicDayThemePreference extends DynamicThemePreference {
 
-    public DynamicNightThemePreference(@NonNull Context context) {
+    public DynamicDayThemePreference(@NonNull Context context) {
         super(context);
     }
 
-    public DynamicNightThemePreference(@NonNull Context context, @Nullable AttributeSet attrs) {
+    public DynamicDayThemePreference(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public DynamicNightThemePreference(@NonNull Context context,
+    public DynamicDayThemePreference(@NonNull Context context,
             @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-    }
-
-    @Override
-    protected void onInflate() {
-        super.onInflate();
-
-        setEntries(getResources().getStringArray(R.array.ads_theme_entries_night));
-        setValues(getResources().getStringArray(R.array.ads_theme_values_night));
-
-        if (getValues() != null && getPreferenceValue() != null) {
-            setDefaultValue(Arrays.asList(getValues()).indexOf(DynamicSdkUtils.is28()
-                    ? Theme.Night.ToString.SYSTEM : Theme.Night.ToString.AUTO));
-
-            updateValueString(false);
-        }
     }
 
     @Override
