44cf1fef62cdc5cfb965827dbae3b8494f5866c0
Pranav Pandey <pranavpande92@gmail.com>
2022-08-12 20:10:45 +0530

    Add color menu preference
    Improve color compact preference.

--- dynamic-support/src/main/java/com/pranavpandey/android/dynamic/support/setting/DynamicColorMenu.java
+++ dynamic-support/src/main/java/com/pranavpandey/android/dynamic/support/setting/DynamicColorMenu.java
@@ -20,42 +20,48 @@ import android.content.Context;
 import android.util.AttributeSet;
 
 import androidx.annotation.AttrRes;
+import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.pranavpandey.android.dynamic.support.Dynamic;
-import com.pranavpandey.android.dynamic.support.setting.base.DynamicColorPreference;
+import com.pranavpandey.android.dynamic.support.R;
 
 /**
- * A compact version of the {@link DynamicColorPreference}.
+ * An {@link android.view.MenuItem} version of the {@link DynamicColorCompact}.
  */
-public class DynamicColorCompact extends DynamicColorPreference {
+public class DynamicColorMenu extends DynamicColorCompact {
 
-    public DynamicColorCompact(@NonNull Context context) {
+    public DynamicColorMenu(@NonNull Context context) {
         super(context);
     }
 
-    public DynamicColorCompact(@NonNull Context context, @Nullable AttributeSet attrs) {
+    public DynamicColorMenu(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public DynamicColorCompact(@NonNull Context context,
+    public DynamicColorMenu(@NonNull Context context,
             @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected @LayoutRes int getLayoutRes() {
+        return R.layout.ads_preference_simple_compact;
     }
 
     @Override
     protected void onUpdate() {
         super.onUpdate();
 
-        Dynamic.setVisibility(getTitleView(), GONE);
-        Dynamic.setVisibility(getSummaryView(), GONE);
         Dynamic.setVisibility(getValueView(), GONE);
-        Dynamic.setVisibility(getDescriptionView(), GONE);
-        Dynamic.setVisibility(getActionView(), GONE);
-        Dynamic.setVisibility(getIconFooterView(), GONE);
 
-        Dynamic.setVisibility(getIconView(), VISIBLE);
-        Dynamic.setVisibility(getValueView(), VISIBLE);
+        if (getIcon() != null) {
+            Dynamic.setVisibility(getIconView(), VISIBLE);
+            Dynamic.setVisibility(getViewFrame(), GONE);
+        } else {
+            Dynamic.setVisibility(getIconView(), GONE);
+            Dynamic.setVisibility(getViewFrame(), VISIBLE);
+        }
     }
 }
